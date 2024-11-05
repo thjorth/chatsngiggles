77b69d8d9f8986b023fbe7c98f34cce37ff1c055
thomas.hjorth <dkthohj@coloplast.com>
2024-11-05 16:21:33 +0100

    x

--- index.js
+++ index.js
@@ -1,4 +1,16 @@
 
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("Express on Vercel"));
+
+//app.get("/test/", (req, res) => res.send("This is a test"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
+
+
 const {
 	GoogleGenerativeAI,
 	HarmCategory,
