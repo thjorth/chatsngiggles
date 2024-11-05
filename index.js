6ae01b7a531c1981f43784696a5c26bc34cd15f9
thomas.hjorth <dkthohj@coloplast.com>
2024-11-05 16:22:46 +0100

    x

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.get("/", (req, res) => res.send("Express on Vercel #2"));
 
 //app.get("/test/", (req, res) => res.send("This is a test"));
 
