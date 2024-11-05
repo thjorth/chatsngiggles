6e7bef614058cef9a80d7bd176c1f9c55350a6af
thomas.hjorth <dkthohj@coloplast.com>
2024-11-05 15:15:01 +0100

    trials

---
+++ api/test.js
@@ -0,0 +1,11 @@
+const express = require("express");
+const app = express();
+
+app.get("/", (req, res) => res.send("This is a test #0"));
+
+app.get("/test/", (req, res) => res.send("This is a test"));
+
+app.listen(3000, () => console.log("Server ready on port 3000."));
+
+module.exports = app;
+
