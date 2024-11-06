84bc3aa5eeb0cb20ef1b9ecee6cc1dc73c6ea6f8
thomas.hjorth <dkthohj@coloplast.com>
2024-11-06 11:01:21 +0100

    x

--- api/index.js
+++ api/index.js
@@ -3,9 +3,7 @@ const app = express();
 
 app.get("/", (req, res) => res.send("Express on Vercel #1"));
 
-app.get("/test", (req, res) => res.send("<h1>This is a test</h1>"));
-
-app.listen(3000, () => console.log("Server ready on port 3000."));
+//app.listen(3000, () => console.log("Server ready on port 3000."));
 
 module.exports = app;
 
